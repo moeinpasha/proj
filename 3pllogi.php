0d5a0927c43b3896b401491ef252f83cc754fd8a
moeinpasha <78461722+moeinpasha@users.noreply.github.com>
2022-02-03 19:23:12 +0530

    Add files via upload

---
+++ 3pllogi.php
@@ -0,0 +1,58 @@
+<?php
+include"header.php";
+?>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container" >
+		<div class="row" style="margin-top:80px;">
+			<div class="col-md-12">
+				<h2 style="text-align: center;text-decoration: underline;">3PL Logistics</h2>
+				<img src="assets/img/logo1.jpg" class="img-fluid" style="width:100%">
+				<p style="font-size:18px;font-family: sans-serif;font-weight:bold;">Logistics is important in any business, offering a comprehensive service that includes pickups, deliveries and shipments using a range of methods and vehicles. Your shipments will be in good hands. fast and reliable logistics company offering a huge range of services for the best prices. Our services include transportation and shipping, domestic. We aim to be the leading logistics company we do not take any risk regarding the safety of our Shipment. We offer a special dashboard for managing shipments and undertaking all the necessary documentation. reliable payment solutions.</p>
+				<div class="row">
+					<div class="col-md-6">
+						<ol type="I">
+					<li>Multifunctional Dashboard</li>
+	<li>Door pickup & Door delivery</li>
+	<li>Safety & security of your shipments</li>
+	<li>Ease Returns</li>
+	<li>Real time data sync</li>
+	<li>Track & Trace</li>
+	<li>Digital Payment solution</li>
+	<li>Auto generate reports</li>
+	<li>Ease invoicing</li>
+	<li>Shipment calculator</li>
+</ol>
+					</div>
+					<div class="col-md-6">
+						<ol type="I">
+							<li>Pincode availability</li> 
+	<li>Faster weight discrepancy resolution</li>
+	<li>Detail payment history</li> 
+	<li>Email & SMS notification</li> 
+	<li>Affordable price</li>
+	<li>Regular updates</li>
+	<li>Complaints reports</li>
+	<li>On time delivery</li>
+	<li>Special Support customer relationship officer</li>
+	<li>Superior customer support</li>
+						</ol>
+					</div>
+				</div>
+				
+	
+
+				
+			</div>
+		</div>
+	</div>
+	</body>
+<?php
+include"footer.php";
+?>
